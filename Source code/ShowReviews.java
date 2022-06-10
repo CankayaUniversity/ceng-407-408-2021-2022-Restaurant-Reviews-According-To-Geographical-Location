99be6522d42fc8efc5e9bd83922e42922864c59f
Coşkun Oruç <93541816+TheNewCoder01@users.noreply.github.com>
2022-06-11 00:15:17 +0300

    Update ShowReviews.java

--- Source code/ShowReviews.java
+++ Source code/ShowReviews.java
@@ -1,23 +1,35 @@
 package com.Coskun.eatie;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class ShowReviews extends AppCompatActivity {
 
     private RecyclerView rV;
     private ListingForReviews lister;
     private FloatingActionButton newrev;
+    private DocumentReference  documentReference;
+    private FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +39,7 @@ public class ShowReviews extends AppCompatActivity {
         Intent intent=getIntent();
         String UserName=intent.getStringExtra("UserNam");
         String RestName=intent.getStringExtra("ResName");
-        lister=new ListingForReviews(Reviews.GetAllReviewsForRestourant(this,RestName),RestName,this);
-        rV.setHasFixedSize(true);
+        ShowAll(RestName,this);
         rV.setLayoutManager(Lm);
         rV.setAdapter(lister);
         newrev=findViewById(R.id.AddRev);
@@ -47,5 +58,40 @@ public class ShowReviews extends AppCompatActivity {
         c.putExtra("UserNam",UserName);
         c.putExtra("ResName",ResName);
         startActivity(c);
+    }
+    private void ShowAll(String RestName, Context context){
+        firestore=FirebaseFirestore.getInstance();
+        ArrayList<Reviews> allReviews=new ArrayList<>();
+        CollectionReference collectionReference=firestore.collection("Yorum").document(RestName).collection("Yorumlar");
+        Query query = collectionReference.whereEqualTo("RestourantName", RestName);
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult())
+                {
+
+                        Reviews c=new Reviews(document.getData().get("UserName").toString(),document.getData().get("yorum").toString(),
+                               document.getData().get("RestourantName").toString(),Integer.parseInt(document.getData().get("Rate").toString()),
+                                Integer.parseInt(document.getData().get("YorumP").toString()));
+                        allReviews.add(c);
+                }
+                    lister=new ListingForReviews(allReviews,RestName,context);
+                    rV.setHasFixedSize(true);
+                    rV.setAdapter(lister);
+
+                    lister.SetOnItemClickListener(new ListingForReviews.OnClickFunc1Listener() {
+                        @Override
+                        public void OnItemClick1() {
+                            Refr();
+                        }
+                    });
+             }
+
+            }
+        });
+    }
+    private void Refr(){
+
     }
 }
